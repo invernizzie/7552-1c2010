26464e9bc95759e79bc695e02101340b180e67c7
invernizzie@gmail.com <invernizzie@gmail.com@21b166b2-2ecd-11df-9ac8-ff20597b2c9b>
2010-04-08 15:36:33 +0000

    Sigo adaptando lo que hizo Cristian al modelo que había planteado

--- trunk/implementacion/model/filters/masks/impl/SimpleMask.java
+++ trunk/implementacion/model/filters/masks/impl/SimpleMask.java
@@ -1,5 +1,6 @@
 package model.filters.masks.impl;
 
+import model.filters.masks.impl.Convolver;
 import model.filters.masks.Mask;
 
 import java.awt.*;
@@ -11,6 +12,8 @@ import java.awt.*;
 public class SimpleMask implements Mask {
 
     private double[][] coefs;
+    private int width, height;
+    int imgpixels[], newimgpixels[];
 
     public static SimpleMask create(double[][] coefs) throws InvalidMaskException {
 
@@ -21,8 +24,8 @@ public class SimpleMask implements Mask {
     }
 
     public Image apply(Image image) {
-        // TODO Copiar implementacion de Cristian
-        return null;
+        SimpleMaskConvolver convolver = new SimpleMaskConvolver();
+        return convolver.filter(image);
     }
 
     private static void testCreate(double[][] coefs) throws InvalidMaskException {
@@ -35,5 +38,52 @@ public class SimpleMask implements Mask {
         for(int i = 0; i < coefs.length; i++)
             if (coefs[i].length != height)
                 throw new InvalidMaskException();
+    }
+
+    private class SimpleMaskConvolver extends Convolver {
+
+        @Override
+        public void convolve() {
+
+            // TODO Cambiar lo que hace aca (es Sharpen) por la aplicacion de una mascara cualquiera
+
+            int  r0=0, g0=0, b0=0;
+            for(int y=1; y<height-1; y++) {
+                for(int x=1; x<width-1; x++) {
+                    int rs = 0;
+                    int gs = 0;
+                    int bs = 0;
+
+                    for(int k=-1; k<=1; k++) {
+                        for(int j=-1; j<=1; j++){
+                            int rgb = imgpixels[(y+k)*width+x+j];
+                            int r = (rgb >> 16) & 0xff;
+                            int g = (rgb >> 8) & 0xff;
+                            int b = rgb & 0xff;
+                            if (j == 0 && k == 0) {
+                                r0 = r;
+                                g0 = g;
+                                b0 = b;
+                            } else {
+                                rs += r;
+                                gs += g;
+                                bs += b;
+                            }
+                        }
+                    }
+
+                    rs >>= 3;
+                    gs >>= 3;
+                    bs >>= 3;
+
+                    newimgpixels[y*width+x] = (0xff000000 |
+                            clamp(r0+r0-rs) << 16 | clamp(g0+g0-gs) << 8 | clamp(b0+b0-bs));
+                }
+            }
+        }
+
+        private final int clamp(int c) {
+            return ( c > 255 ? 255 : (c < 0 ? 0 : c));
+        }
     }
 }
