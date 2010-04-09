e7aee6b783864a05b3c9f7dcd13f62ca24e95bdc
invernizzie <invernizzie@21b166b2-2ecd-11df-9ac8-ff20597b2c9b>
2010-04-09 01:31:11 +0000

    Sigo adaptando lo que hizo Cristian al diseño que hice al principio

--- implementacion/model/filters/masks/impl/SimpleMask.java
+++ implementacion/model/filters/masks/impl/SimpleMask.java
@@ -47,37 +47,28 @@ public class SimpleMask implements Mask {
 
             // TODO Cambiar lo que hace aca (es Sharpen) por la aplicacion de una mascara cualquiera
 
-            int  r0=0, g0=0, b0=0;
             for(int y=1; y<height-1; y++) {
                 for(int x=1; x<width-1; x++) {
-                    int rs = 0;
-                    int gs = 0;
-                    int bs = 0;
+                    int newR = 0;
+                    int newG = 0;
+                    int newB = 0;
 
                     for(int k=-1; k<=1; k++) {
                         for(int j=-1; j<=1; j++){
-                            int rgb = imgpixels[(y+k)*width+x+j];
+                            int rgb = imgpixels[ (y+k)*width + (x+j) ];
                             int r = (rgb >> 16) & 0xff;
                             int g = (rgb >> 8) & 0xff;
                             int b = rgb & 0xff;
-                            if (j == 0 && k == 0) {
-                                r0 = r;
-                                g0 = g;
-                                b0 = b;
-                            } else {
-                                rs += r;
-                                gs += g;
-                                bs += b;
-                            }
+
+                            newR += r * coefs[j+1][k+1];
+                            newG += g * coefs[j+1][k+1];
+                            newB += b * coefs[j+1][k+1];
                         }
                     }
-
-                    rs >>= 3;
-                    gs >>= 3;
-                    bs >>= 3;
-
                     newimgpixels[y*width+x] = (0xff000000 |
-                            clamp(r0+r0-rs) << 16 | clamp(g0+g0-gs) << 8 | clamp(b0+b0-bs));
+                    clamp(newR) << 16 | clamp(newG) << 8 | clamp(newB));
+                    /* TODO Mover el clamping a la mascara normalizada
+                    newimgpixels[y*width+x] = (0xff000000 | newR << 16 | newG << 8 | newB); */
                 }
             }
         }
