3f6efffe6dd4c4a0e9a703b0d29199982d9ead89
cristianlissauer <cristianlissauer@21b166b2-2ecd-11df-9ac8-ff20597b2c9b>
2010-05-25 21:48:17 +0000

    FFT

--- trunk/src/control/command/CommandDFT.java
+++ trunk/src/control/command/CommandDFT.java
@@ -1,12 +1,28 @@
 package control.command;
 
+import java.awt.image.BufferedImage;
+
+import model.fouriertransform.ImageFFT;
+import model.fouriertransform.exceptions.FFTException;
 import control.command.exceptions.CommandExecutionException;
 
 
 public class CommandDFT extends MyFrameCommand {
 
-    protected void doExecute() throws CommandExecutionException {
-    	//TODO
+    protected void doExecute() throws CommandExecutionException{
+    	BufferedImage bi = new BufferedImage(
+                frame.getImage().getWidth(null),
+                frame.getImage().getHeight(null),
+                BufferedImage.TYPE_BYTE_GRAY);
+    	try{
+    		ImageFFT fft = new ImageFFT(bi,ImageFFT.HANNING_WINDOW);
+    		fft.transform();
+    		frame.setImage(fft.getSpectrum());
+	        frame.repaint();
+	        
+    	}catch (FFTException e) {
+    		e.printStackTrace();
+    	}
     }
 
     protected void testExecute() throws CommandExecutionException {
