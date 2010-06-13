db075db84e19d1c24125cbb0608b4d4e068ed855
nanozarate@gmail.com <nanozarate@gmail.com@21b166b2-2ecd-11df-9ac8-ff20597b2c9b>
2010-06-13 17:35:55 +0000

    Subi la DFT y IDFT y el test, y tambien el comienzo de la lista de filtros guardados(falta terminar)

---
+++ src/control/command/CommandApplySavedFilterList.java
@@ -0,0 +1,12 @@
+package control.command;
+
+import control.command.exceptions.CommandExecutionException;
+
+
+public class CommandApplySavedFilterList extends HandlerCommand {
+
+    public void doExecute() throws CommandExecutionException {
+    	//TODO aplicar los filtros guardados
+    }
+
+}
--- src/model/filters/Filter.java
+++ src/model/filters/Filter.java
@@ -4,5 +4,5 @@ import java.awt.Image;
 
 public interface Filter {
 	public Image filter(Image image);
-	
+
 }
