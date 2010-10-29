4b038016706213b80c15378b4d136bc1d3b071af
nanozarate@gmail.com <nanozarate@gmail.com@21b166b2-2ecd-11df-9ac8-ff20597b2c9b>
2010-10-29 15:01:47 +0000

    Commitie un par de cosas, OJO porque no termina de andar todo, solo lo subo desde mi trabajo para despues seguirlo desde mi casa

--- trunk/src/control/command/CommandApplyFilter.java
+++ trunk/src/control/command/CommandApplyFilter.java
@@ -1,9 +1,6 @@
 package control.command;
 
 import java.awt.Image;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
 
 import model.filters.Filter;
 import control.command.exceptions.CommandExecutionException;
@@ -36,29 +33,4 @@ public class CommandApplyFilter extends MyFrameCommand {
         this.filter = filter;
     }
 
-    public static String[] extractFilterNames(String sequence) {
-        boolean defaultParameters = false;
-    	StringTokenizer tokenizer = new StringTokenizer(sequence, "-");
-    	List<String> filters = new ArrayList<String>();
-    	while(tokenizer.hasMoreTokens()){
-    		String token = tokenizer.nextToken();
-    		if(!"true".equals(token) && !"false".equals(token)) {
-    			filters.add(token);
-    		}
-    	}
-        return filters.toArray(new String[1]);
-    }
-
-    public static boolean determineParametricsChoice(String sequence) {
-        boolean defaultParameters = false;
-    	StringTokenizer tokenizer = new StringTokenizer(sequence, "-");
-    	List<String> filters = new ArrayList<String>();
-    	while(tokenizer.hasMoreTokens()){
-            if (tokenizer.nextToken().equals("true")){
-    			return true;
-    		}
-
-    	}
-        return false;
-    }
 }
